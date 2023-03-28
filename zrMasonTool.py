108dfee46c095495513ffa36d0db9af1cb763cd7
Sonicbird21 <67420120+Sonicbird21@users.noreply.github.com>
2023-03-28 12:53:16 +0200

    Update zrMasonTool.py

--- zrMasonTool.py
+++ zrMasonTool.py
@@ -7,7 +7,7 @@ from colorama import Fore, Style
 
 ws_url = "wss://mason-ipv4.zombsroyale.io/gateway/?EIO=4&transport=websocket"
 
-userKey = "ChangeHere"
+
 status = "online"
 mode = "Solo"
 
@@ -15,7 +15,7 @@ connect_messages = {
     "setPlat": '42["setPlatform", "android"]',
     "setVer": '42["setVersion", "4.7.0"]',
     "setName": '42["setName", "Player"]',
-    "login": f'42["login", "{userKey}"]',
+    "login": f'42["login", "null"]',
     "status": f'42["setStatus", "{status}"]',
 }
 
@@ -28,6 +28,21 @@ party_messages = {
 
 party_leave_message = '42["leaveParty"]'
 
+def check_userKey(userKey):
+    try:
+        r = requests.get(f"https://zombsroyale.io/user/{userKey}")
+        if r.status_code != 200:
+            print(Fore.RED + "Invalid UserKey!" + Style.RESET_ALL)
+            return False
+    except Exception as e:
+        print(Fore.RED + f"Error connecting to API: {e}" + Style.RESET_ALL)
+        return False
+    friend_code = r.json()['user']['friend_code']
+    print(Fore.GREEN + "Valid User Key!" + Style.RESET_ALL)
+    connect_messages["login"] = f'42["login", "{userKey}"]'
+    print(Fore.GREEN + f"Account Key set (Current Key: '{userKey}') [User:{friend_code}]" + Style.RESET_ALL)
+    return True
+    
 
 async def create_party(ws):
     for message_type, message in party_messages.items():
@@ -100,19 +115,33 @@ async def main():
                 print(Fore.RED + "Already connected to WebSocket" + Style.RESET_ALL)
             else:
                 try:
+
+                    if connect_messages["login"] == '42["login", "null"]':
+                        print(Fore.RED + "No account key set" + Style.RESET_ALL)
+                        continue
+
+
+
                     ws = await websockets.connect(ws_url)                    
 
                     if debug_mode:
                         for message_type, message in connect_messages.items():
                             await ws.send(message)
+
                             print(Fore.YELLOW + f"Sent message: {message}" + Style.RESET_ALL)
                             if message_type != "setName":
-                                response = await ws.recv()
-                                print(Fore.GREEN + f"Received response: {response}" + Style.RESET_ALL)
+                                try:
+                                    response = await asyncio.wait_for(ws.recv(), timeout=3.0)
+                                    print(Fore.GREEN + f"Received response: {response}" + Style.RESET_ALL)
+                                except asyncio.TimeoutError:
+
+                                    continue
                     print(Fore.GREEN + "WebSocket connected" + Style.RESET_ALL)
 
+                   
                 except Exception as e:
-                    print(Fore.RED + f"Error connecting to WebSocket: {e}" + Style.RESET_ALL)
+                        print(Fore.RED + f"Error connecting to WebSocket: {e}" + Style.RESET_ALL)
+
         elif user_input == "2":
             try:
                 if ws:
@@ -125,18 +154,10 @@ async def main():
         elif user_input == "3":
             userKey = input(Fore.WHITE + "Enter account key: ")
 
-            try:
-                r = requests.get(f"https://zombsroyale.io/user/{userKey}")
-                if r.status_code != 200:
-                    print(Fore.RED + "Invalid UserKey!" + Style.RESET_ALL)
-                    continue
-            except Exception as e:
-                print(Fore.RED + f"Error connecting to API: {e}" + Style.RESET_ALL)
+            if not check_userKey(userKey):
                 continue
-            friend_code = r.json()['user']['friend_code']
-            print(Fore.GREEN + "Valid User Key!" + Style.RESET_ALL)
-            connect_messages["login"] = f'42["login", "{userKey}"]'
-            print(Fore.GREEN + f"Account Key set (Current Key: '{userKey}') [User:{friend_code}]" + Style.RESET_ALL)
+
+            
         elif user_input == "debug on":
             debug_mode = True
             print(Fore.GREEN + "Debug mode is on." + Style.RESET_ALL)
